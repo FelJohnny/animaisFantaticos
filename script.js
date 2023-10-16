af8f7528693d4257aa7eccd15d0d54805e9150d8
Felipe Johnny <felipijohnny@outlook.com>
2023-10-16 09:49:55 -0300

    aplicando animações

--- script.js
+++ script.js
@@ -1,20 +1,36 @@
-const tabMenu = document.querySelectorAll('.js-tabmenu li');
-const tabContent = document.querySelectorAll('.js-tabcontent section');
+function tabMenu() {
 
-if(tabMenu.length && tabContent.length){
-    tabContent[0].classList.add('ativo');
-function activeTab(index){
-    tabContent.forEach((section) =>{
-        section.classList.remove('ativo');
-    })
-    tabContent[index].classList.add('ativo');
+    const tabMenu = document.querySelectorAll('.js-tabmenu li');
+    const tabContent = document.querySelectorAll('.js-tabcontent section');
+
+    if(tabMenu.length && tabContent.length){
+        tabContent[0].classList.add('ativo');
+        function activeTab(index){
+            tabContent.forEach((section) =>{
+                section.classList.remove('ativo');
+            })
+        tabContent[index].classList.add('ativo');
+    }
+
+    tabMenu.forEach((itemMenu, index)=>{
+        itemMenu.addEventListener('click', () =>{
+            console.log(tabMenu[index])
+            activeTab(index);
+        })
+    });
+
+    }
+
 }
+tabMenu();
 
-tabMenu.forEach((itemMenu, index)=>{
-    itemMenu.addEventListener('click', () =>{
-        console.log(tabMenu[index])
-        activeTab(index);
-    })
-});
+const accordiontlist = document.querySelectorAll('.js-accordion dt');
 
+function activeAccordion(){
+    this.classList.toggle('ativo');
+    this.nextElementSibling.classList.toggle('ativo');
 }
+accordiontlist.forEach((item) =>{
+    item.addEventListener('click', activeAccordion);
+    
+})
