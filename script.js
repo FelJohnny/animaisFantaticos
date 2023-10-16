66a58a7b92eca7ed7d7d781c676324b0aa10d0b5
Felipe Johnny <felipijohnny@outlook.com>
2023-10-16 13:47:09 -0300

    aplicando animação ao rolar a pagina

--- script.js
+++ script.js
@@ -60,17 +60,22 @@ function scrollAnimation(){
 }
 scrollAnimation();
 
-
-const sections = document.querySelectorAll('.js-scroll');
-const windowMetade = window.innerHeight * 0.6;
-function transitionScroll(){
-    sections.forEach((section) =>{
-        const sectionTop = section.getBoundingClientRect().top;
-        const sectionVisible = sectionTop - windowMetade < 0;
-        if(sectionVisible){
-            section.classList.add('ativo');
+function validaScrollAnima(){
+    const sections = document.querySelectorAll('.js-scroll');
+    if(sections.length){
+        const windowMetade = window.innerHeight * 0.7;
+        function transitionScroll(){
+            sections.forEach((section) =>{
+                const sectionTop = section.getBoundingClientRect().top;
+                const sectionVisible = sectionTop - windowMetade < 0;
+                if(sectionVisible){
+                    section.classList.add('ativo');
+                }
+            });
         }
-    })
-}
 
-window.addEventListener('scroll', transitionScroll)
+        transitionScroll();
+        window.addEventListener('scroll', transitionScroll);
+    }
+}
+validaScrollAnima();
